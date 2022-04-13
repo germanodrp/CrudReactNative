ced1ea909ced7ba239106a264b1413517eb50261
Germano <germanodrp18@gmail.com>
2022-04-13 14:00:18 -0300

    novo

--- App.js
+++ App.js
@@ -1,14 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [pais, setPais] = useState('')
+
+
+  async function buscarPais(){
+    const retornoPais = await api.get('/pais')
+    setPais(retornoPais.nomePais)
+  }
+
+
+  const clear = () => {
+      setPais("")
+      
+    / setInfo()
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.viewContainer}>
+    <Text style={styles.text}>Pais : </Text>
+   <TextInput
+     style={styles.textInput}
+     onChangeText={pais => setPais(pais)}
+     value={pais}
+     placeholder='Exemplo: Alemanha'
+     keyboardType={'string'}
+   />
+   <separator/>
+  
+   <Separator />
+   <Button
+     onPress={()=>buscarPais()}
+     title='Buscar'
+     color='green'
+     accessibilityLabel='Clique aqui para Buscar Pais'
+   />
+   <Separator/>
+   <Button
+     onPress={clear}
+     title='Limpa'
+     color='red'
+     accessibilityLabel='Botão para limpar os valores'
+   />
+   <Separator />
+   <Text style={styles.input}>
+    Pais é: {pais} 
+   </Text>
+ </View>
   );
 }
+const Separator = () => (
+  <View style={styles.separator} />
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +62,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 5,
+    fontSize: 25,
+  },
+  separator: {
+    marginVertical: 10,
+    borderBottomColor: 'black',
+    borderWidth: 1,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  textInput: {
+    height: 50,
+    borderWidth: 2,
+    borderColor: 'black',
+    paddingLeft: 10,
+    margin: 15,
+    borderRadius: 20
+  }
 });
---
+++ service.js
@@ -0,0 +1,6 @@
+
+
+const api = axios.create({
+    baseUrl:''
+}
+)
